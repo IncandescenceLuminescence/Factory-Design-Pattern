7c6657b144c990101b3c8fb07119c68a20f5f887
IncandescenceLuminescence <60149069+IncandescenceLuminescence@users.noreply.github.com>
2020-02-13 20:42:18 -0500

    Add files via upload

---
+++ GroceryStore.java
@@ -0,0 +1,18 @@
+package csce247.assignments.factory;
+public class GroceryStore {	
+	public Cereal orderCereal(String type) {
+		Cereal cereal=null;
+		if(type.equals("frosted flakes"))
+			cereal=new FrostedFlakes();
+		else if(type.equals("lucky charms"))
+			cereal=new LuckyCharms();
+		else if(type.equals("fruit loops"))
+			cereal=new FruitLoops();	
+		else
+			return null;
+		cereal.prepare();
+		cereal.boxCereal();
+		cereal.priceCereal();
+		return cereal;
+	}
+}
